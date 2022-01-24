5e814c4e52e9f2edc2679f4d078f3eb7adaf861e
Rene Bauta <17625558+rrbauta@users.noreply.github.com>
2022-01-24 07:26:10 -0500

    Initial migration
    Database creation
    Insert Destinations and Contact Types
    Create a store procedure to create a contact

--- IsuReservation/Migrations/20220124121014_InitialCreate.cs
+++ IsuReservation/Migrations/20220124121014_InitialCreate.cs
@@ -87,13 +87,34 @@ namespace IsuReservation.Migrations
                         name: "FK_Reservations_Contacts_ContactId",
                         column: x => x.ContactId,
                         principalTable: "Contacts",
-                        principalColumn: "Id");
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
                     table.ForeignKey(
                         name: "FK_Reservations_Destinations_DestinationId",
                         column: x => x.DestinationId,
                         principalTable: "Destinations",
                         principalColumn: "Id",
                         onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "ContactTypes",
+                columns: new[] { "Id", "DateCreated", "DateModified", "IsDeleted", "Name" },
+                values: new object[,]
+                {
+                    { new Guid("2e5c86eb-ec54-44ad-bac5-e6928203cf85"), new DateTime(2022, 1, 24, 7, 10, 14, 696, DateTimeKind.Local).AddTicks(2240), new DateTime(2022, 1, 24, 7, 10, 14, 696, DateTimeKind.Local).AddTicks(2241), false, "Contact Type 3" },
+                    { new Guid("5aa58205-0b51-4ec8-9822-8e61c728817c"), new DateTime(2022, 1, 24, 7, 10, 14, 696, DateTimeKind.Local).AddTicks(2214), new DateTime(2022, 1, 24, 7, 10, 14, 696, DateTimeKind.Local).AddTicks(2215), false, "Contact Type 2" },
+                    { new Guid("64cb848f-43d6-47a6-a05d-2141a2aabdd4"), new DateTime(2022, 1, 24, 7, 10, 14, 696, DateTimeKind.Local).AddTicks(2186), new DateTime(2022, 1, 24, 7, 10, 14, 696, DateTimeKind.Local).AddTicks(2188), false, "Contact Type 1" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Destinations",
+                columns: new[] { "Id", "DateCreated", "DateModified", "Description", "Favorite", "Image", "IsDeleted", "Name", "Rating" },
+                values: new object[,]
+                {
+                    { new Guid("5e1d7f3d-210c-44e3-8854-a642d956aa55"), new DateTime(2022, 1, 24, 7, 10, 14, 696, DateTimeKind.Local).AddTicks(1911), new DateTime(2022, 1, 24, 7, 10, 14, 696, DateTimeKind.Local).AddTicks(1937), "Description for Destination 1", false, "https://as01.epimg.net/diarioas/imagenes/2021/03/08/actualidad/1615216406_061390_1615221159_sumario_normal.jpg", false, "Destination 1", 5 },
+                    { new Guid("cbe11d8e-424e-4776-a536-2e7f5e290041"), new DateTime(2022, 1, 24, 7, 10, 14, 696, DateTimeKind.Local).AddTicks(2040), new DateTime(2022, 1, 24, 7, 10, 14, 696, DateTimeKind.Local).AddTicks(2041), "Description for Destination 3", false, "https://viajes.nationalgeographic.com.es/medio/2019/04/30/istock-840449198_07c3ef3b_1245x842.jpg", false, "Destination 3", 1 },
+                    { new Guid("f0d0aa79-399a-4283-bd2f-acbf3eb537dc"), new DateTime(2022, 1, 24, 7, 10, 14, 696, DateTimeKind.Local).AddTicks(2013), new DateTime(2022, 1, 24, 7, 10, 14, 696, DateTimeKind.Local).AddTicks(2014), "Description for Destination 2", false, "https://viajes.nationalgeographic.com.es/medio/2019/04/30/istock-840449198_07c3ef3b_1245x842.jpg", false, "Destination 2", 1 }
                 });
 
             migrationBuilder.CreateIndex(
@@ -110,6 +131,24 @@ namespace IsuReservation.Migrations
                 name: "IX_Reservations_DestinationId",
                 table: "Reservations",
                 column: "DestinationId");
+            
+            var sp = @"CREATE PROCEDURE [dbo].[CreateContact]
+                     @Id uniqueidentifier,                      
+                     @Name varchar(50),
+                     @PhoneNumber varchar (50),
+                     @BirthDate datetime2,
+                     @ContactTypeId uniqueidentifier,
+                     @IsDeleted bit,
+                     @DateCreated datetime2,
+                     @DateModified datetime2
+                AS
+                BEGIN
+                    SET NOCOUNT ON;
+                    Insert into Contacts([Id],[Name],[PhoneNumber],[BirthDate],[ContactTypeId],[IsDeleted],[DateCreated],[DateModified])
+                    Values (@Id, @Name, @PhoneNumber, @BirthDate, @ContactTypeId, @IsDeleted, @DateCreated, @DateModified)
+                END";
+
+            migrationBuilder.Sql(sp);
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
